2166a7a5302b29b64ce64ea54305a96603076855
abhishekbharti <abhishek.bharti@testbook.com>
2019-03-15 18:12:39 +0530

    Initial commit

---
+++ feature/src/androidTest/java/io/github/abhishekbhartiprojects/mysociety/feature/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package io.github.abhishekbhartiprojects.mysociety.feature
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals(
+            "io.github.abhishekbhartiprojects.mysociety.feature.test",
+            appContext.packageName
+        )
+    }
+}
